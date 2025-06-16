e10e55b2cb42bc2fd19b01fb99ec7ec48c097b37
Tristan <tristan.latuilliere@gmail.com>
2025-06-16 12:19:29 +0000

    first try

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main : Start of programm
+ *
+ * Description : Prints the text in printf
+ *
+ * Return : 0
+ */
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0)
+}
