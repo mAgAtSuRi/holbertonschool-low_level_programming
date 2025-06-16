d1605681a79b1e81cffe886f15847b27f7297fae
Tristan <tristan.latuilliere@gmail.com>
2025-06-16 13:13:38 +0000

    ld into lu

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -15,11 +15,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %ld byte(s)", (unsigned long)sizeof(c));
-	printf("Size of a char: %ld byte(s)", (unsigned long)sizeof(i));
-	printf("Size of a char: %ld byte(s)", (unsigned long)sizeof(li));
-	printf("Size of a char: %ld byte(s)", (unsigned long)sizeof(lli));
-	printf("Size of a char: %ld byte(s)", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(c));
+	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(i));
+	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(li));
+	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(lli));
+	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(f));
 
 	return(0);
 }
