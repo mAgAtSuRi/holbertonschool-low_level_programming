1cb2cd2c511a20a9a4cd80df2f915a0444bbf538
Tristan <tristan.latuilliere@gmail.com>
2025-06-17 13:15:22 +0000

    && replaced by ||

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,15 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = i+1 ; j <=9 ; j++)
+		for (j = i + 1 ; j <= 9 ; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
