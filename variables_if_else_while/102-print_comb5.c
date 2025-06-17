844a78b52595a13c4fcb2d57e18761c04982ecee
Tristan <tristan.latuilliere@gmail.com>
2025-06-17 14:03:14 +0000

    first try

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description - print all combinations of two 2 digit numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n1;
+	int n2;
+	int n3;
+	int n4;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = 0; n2 <= 9; n2++)
+		{
+			for (n3 = n1; n3 <= 9; n3++)
+			{
+				for (n4 = n2 + 1; n4 <= 9; n4++)
+				{
+					putchar(n1 + '0');
+					putchar(n2 + '0');
+					putchar(' ');
+					putchar(n3 + '0');
+					putchar(n4 + '0');
+					if (n1 != 9 || n2 != 8 || n3 != 9 || n4 != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
