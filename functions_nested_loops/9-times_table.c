133a07ef71a2c8d642f4723d1f896d65464d2956
Tristan <tristan.latuilliere@gmail.com>
2025-06-18 14:17:38 +0000

    corrected

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,16 +8,30 @@ void times_table(void)
 	int i = 0;
 	int j;
 	int result;
+	int result2;
 
 	for (; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			_putchar(result + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			if (result >= 10)
+			{
+				result2 = result % 10;
+				result = result / 10;
+				_putchar(result + '0');
+				_putchar(result2 + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(result + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
 		}
 	}
 }
