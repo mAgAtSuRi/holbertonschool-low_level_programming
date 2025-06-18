7c4161f96f49ae4ac24ea545bf34c1c15840ffce
Tristan <tristan.latuilliere@gmail.com>
2025-06-18 12:12:56 +0000

    firs try

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ */
+void alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	while (i <= 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+			_putchar('\n');
+		}
+	}
+}
