58e96dad04a795e2e204e50dc0a79062cad2095a
Tristan <tristan.latuilliere@gmail.com>
2025-06-18 09:27:32 +0000

    first try

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}
