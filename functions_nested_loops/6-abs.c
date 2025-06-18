d761b908cfc925faf2cb28923ceeb8f6a2ef2ced
Tristan <tristan.latuilliere@gmail.com>
2025-06-18 12:59:21 +0000

    first try

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * _abs - show the absolute value of a number
+ * @n: number to check
+ *
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
