38838f6ef5ca29ab9ff9bdea3bf313ab897ee38e
Tristan <tristan.latuilliere@gmail.com>
2025-06-18 07:53:20 +0000

    try

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -18,19 +18,23 @@ int main(void)
 	{
 		for (n2 = 0; n2 <= 9; n2++)
 		{
-			for (n3 = n1; n3 <= 9; n3++)
+
+			for (n3 = 0; n3 <= 9; n3++)
 			{
-				for (n4 = n2 + 1; n4 <= 9; n4++)
+				for (n4 = 0; n4 <= 9; n4++)
 				{
-					putchar(n1 + '0');
-					putchar(n2 + '0');
-					putchar(' ');
-					putchar(n3 + '0');
-					putchar(n4 + '0');
-					if (n1 != 9 || n2 != 8 || n3 != 9 || n4 != 9)
-					{
-						putchar(',');
+					if (n3 != n1 || n4 != n2)
+					{	
+						putchar(n1 + '0');
+						putchar(n2 + '0');
 						putchar(' ');
+						putchar(n3 + '0');
+						putchar(n4 + '0');
+						if (n1 != 9 || n2 != 8 || n3 != 9 || n4 != 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 				}
 			}
