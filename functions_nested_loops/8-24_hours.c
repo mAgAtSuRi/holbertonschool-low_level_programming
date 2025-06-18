d53c464f77aeea825b50faa7fcf1536a05fcf4fb
Tristan <tristan.latuilliere@gmail.com>
2025-06-18 13:37:37 +0000

    first try

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute since the start of the day
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 2 ; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 6; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
