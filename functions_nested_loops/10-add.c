4c308ed87dac10e8f2d79dcdfadc0e83ef26af4e
Tristan <tristan.latuilliere@gmail.com>
2025-06-19 07:55:05 +0000

    firsst try"

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @n1: first humber
+ * @n2: seconde number
+ *
+ * Return: sum of the two numbers
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
