f3e9c894b2a4d0e3a5c24a566dac709f0b842d47
Tristan <tristan.latuilliere@gmail.com>
2025-06-19 08:04:28 +0000

    first try

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print all numbers to 98 starting from n
+ * @n: starting number for counting
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+		break;
+	for (i = n; i <= 98; i++)
+	{
+		_putchar(i + '0');
+		if (i != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
