4aef6f1c92b253b84cdf4739f962cd6c34e6589f
Tristan <tristan.latuilliere@gmail.com>
2025-06-20 12:24:27 +0000

    first try

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -13,12 +13,13 @@ int main(void)
 	int i;
 	int sum = 0;
 
-	for (i = 0; i < 1024; i++)
+	for (i = 0; i < 23; i++)
 	{
 		if (i % 3 == 0)
 			sum += i;
 		else if (i % 5 == 0)
 			sum += i;
 	}
+	printf("%d\n", sum);
 	return (0);
 }
