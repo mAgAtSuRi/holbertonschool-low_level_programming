bb883da3b8a922fc6b6ee308dd6a10edcae7eae8
Tristan <tristan.latuilliere@gmail.com>
2025-06-20 13:42:40 +0000

    first try

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description - prints the sum of
+ * even fibonacci numbers below 4 000 000
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	long int num1 = 1;
+	long int num2 = 2;
+	long int sum_even = 0;
+	long int next_n;
+
+	while ((num1 && num2) < 4000000)
+	{
+		if (num1 % 2 == 0)
+			sum_even += num1;
+		else if (num2 % 2 == 0)
+			sum_even += num2;
+		next_n = num1 + num2;
+		num1 = num2;
+		num2 = next_n
+	}
+	printf("%ld", sum_even);
+	printf("\n");
+	return (0);
+}
