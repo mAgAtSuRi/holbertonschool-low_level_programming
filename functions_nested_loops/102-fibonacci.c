441830c3503d57ab4a05e4b2bef1ea34cf0fabc5
Tristan <tristan.latuilliere@gmail.com>
2025-06-20 12:55:34 +0000

    for instead of while

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -9,13 +9,13 @@
  */
 int main(void)
 {
-	int i = 1;
+	int i;
 	long int num1 = 1;
 	long int num2 = 2;
 	long int sum;
 
 	printf("%ld, %ld", num1, num2);
-	while (i <= 50)
+	for (i = 1; i <= 50; i++)
 	{
 		if (i == 0)
 			printf("%ld", num1);
@@ -27,7 +27,6 @@ int main(void)
 			printf(", %ld", sum);
 			num1 = num2;
 			num2 = sum;
-			i++;
 		}
 	}
 	return (0);
