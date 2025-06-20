9f286603ff2385fc7e81bd4a1d35e40751e573aa
Tristan <tristan.latuilliere@gmail.com>
2025-06-20 13:49:42 +0000

    first try

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description - prints the first 50 fibonacci numbers,
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	long int num1 = 1;
+	long int num2 = 2;
+	long int sum;
+
+	for (i = 1; i <= 98; i++)
+	{
+		if (i == 1)
+			printf("%d", i);
+		else if (i == 2)
+			printf(", %d", i);
+		else
+		{
+			sum = num1 + num2;
+			printf(", %ld", sum);
+			num1 = num2;
+			num2 = sum;
+		}
+	}
+	printf("\n");
+	return (0);
+}
