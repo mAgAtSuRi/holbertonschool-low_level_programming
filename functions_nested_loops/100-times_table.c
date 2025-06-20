488816cb4123bcdf4943ed2053e65a5c653e9acd
Tristan <tristan.latuilliere@gmail.com>
2025-06-20 07:45:35 +0000

    formatted

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -11,7 +11,6 @@ void print_times_table(int n)
 	for (i = 0; i <= n; i++)
 	{
 		_putchar('0');
-
 		for (j = 1; j <= n; j++)
 		{
 			result = i * j;
@@ -22,11 +21,19 @@ void print_times_table(int n)
 			{
 				_putchar(' ');
 				_putchar(' ');
+				_putchar(result + '0');
 			}
 			else if (result < 100)
+			{
 				_putchar(' ');
-
-			_putchar(result + '0');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else
+			{
+				_putchar(result / 100 + '0');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
 		}
 		_putchar('\n');
 	}
