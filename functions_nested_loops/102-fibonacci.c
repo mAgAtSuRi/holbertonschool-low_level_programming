444db55e5ad9323c92374db65b7bc70911a0d3bb
Tristan <tristan.latuilliere@gmail.com>
2025-06-20 12:42:32 +0000

    long int

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -14,13 +14,13 @@ int main(void)
 	long int num2 = 2;
 	long int sum;
 
-	printf("%d, %d", num1, num2);
+	printf("%ld, %ld", num1, num2);
 	while (i <= 50)
 	{
 		putchar(',');
 		putchar(' ');
 		sum = num1 + num2;
-		printf("%d", sum);
+		printf("%ld", sum);
 		num1 = num2;
 		num2 = sum;
 		i++;
