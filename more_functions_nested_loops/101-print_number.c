9c00142ac6f6654bf4abf363eec46886751c7a13
Tristan <tristan.latuilliere@gmail.com>
2025-06-23 13:56:30 +0000

    first try

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - print a number
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	int i = 0;
+	int j;
+
+	while (n < 10)
+	{
+		n = n / 10;
+		i++;
+	}
+	_putchar(n + '0');
+	for (j = 0; j < i; j++)
+	{
+		n = n * 10;
+		_putchar(n % 10 + '0');
+	}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -13,5 +13,6 @@ void print_diagonal(int n);
 void print_square(int);
 void print_triangle(int);
 int check_prime(int);
+void print_number(int n);
 
 #endif
