38819bd529138436eb930a7ac65a9352092d1fa6
Tristan <tristan.latuilliere@gmail.com>
2025-06-23 13:27:27 +0000

    all in one

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -8,7 +8,19 @@
  *
  * Return: always 0
  */
-int check_prime(int);
+int check_prime(int n)
+{
+        int i;
+
+        if (n < 2)
+                return (0);
+        for (i = 2; i <= n / 2; i++)
+        {
+                if (n % i == 0)
+                        return (0);
+        }
+        return (1);
+}
 
 int main(void)
 {
