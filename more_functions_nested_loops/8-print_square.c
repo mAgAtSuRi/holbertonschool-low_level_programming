5e4dc62feba08e09ae14345564b5e0871c2b0216
Tristan <tristan.latuilliere@gmail.com>
2025-06-23 09:43:10 +0000

    first try

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_square - print a square with #
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
