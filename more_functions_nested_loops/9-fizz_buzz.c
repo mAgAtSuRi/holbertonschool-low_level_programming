4af6d9412faa47ce56c534e548adf3d61312f8e0
Tristan <tristan.latuilliere@gmail.com>
2025-06-23 10:05:34 +0000

    formatted

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -19,7 +19,7 @@ int main(void)
 		if (i == 1)
 			printf("%d", i);
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf(" Fizz Buzz");
+			printf(" FizzBuzz");
 		else if (i % 3 == 0)
 			printf(" Fizz");
 		else if (i % 5 == 0)
