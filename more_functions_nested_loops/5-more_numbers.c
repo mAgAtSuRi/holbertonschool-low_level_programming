32936f071dccbc554b827a2b6f53e5c6975a894b
Tristan <tristan.latuilliere@gmail.com>
2025-06-23 09:08:34 +0000

    2 figures output corrected

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,12 @@ void more_numbers(void)
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
-			_putchar(j + '0');
+		{
+			if (j <= 9)
+				_putchar(j + '0');
+			else
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
 		_putchar('\n');
 	}
 }
