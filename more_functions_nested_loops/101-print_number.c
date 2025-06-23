235e2163cca9c614b72ffc05af56982fc8198ee9
Tristan <tristan.latuilliere@gmail.com>
2025-06-23 14:10:03 +0000

    float added

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -8,21 +8,23 @@ void print_number(int n)
 {
 	int i = 0;
 	int j;
-	
+	float k;
+
 	if (n < 0)
 	{
 		n = -n;
 		_putchar('-');
 	}
 	while (n >= 10)
-	{
+	{	
+		k = n / 10;
 		n = n / 10;
 		i++;
 	}
 	_putchar(n + '0');
 	for (j = 0; j < i; j++)
 	{
-		n = n * 10;
-		_putchar(n % 10 + '0');
+		k = k * 10;
+		_putchar(k % 10 + '0');
 	}
 }
