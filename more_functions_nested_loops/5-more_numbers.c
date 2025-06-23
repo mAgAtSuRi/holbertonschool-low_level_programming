eae9f7b898227ce301432f5c69fc6f3c04510c69
Tristan <tristan.latuilliere@gmail.com>
2025-06-23 09:05:48 +0000

    small corrections

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,8 +8,10 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 1; i <= 10; i++i)
+	{
 		for (j = 0; j <= 14; j++)
 			_putchar(j + '0');
-		_putchar('/n');
+		_putchar('\n');
+	}
 }
