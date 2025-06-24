9d105021ccb4cf4cbe0303d0ec6020edd4e9a2b3
Tristan <tristan.latuilliere@gmail.com>
2025-06-24 09:13:43 +0000

    \0 removed

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,22 @@ void print_rev(char *s)
 
 	while (s[i] != '\0')
 		i++;
+	i--;
 	for (; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "12 45";
+    print_rev(str);
+    return (0);
 }
