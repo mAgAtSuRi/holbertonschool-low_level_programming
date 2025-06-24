946f831e28d01924eb9168e437e7be99da3e4c5b
Tristan <tristan.latuilliere@gmail.com>
2025-06-24 09:05:49 +0000

    first try

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  print_rev - print a string in reverse
+ *  @s: string to reverse
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (; i > 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
