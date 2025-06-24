36eb4efe315b5e9d0f1bb16c8e89dfea9ab9f86e
Tristan <tristan.latuilliere@gmail.com>
2025-06-24 08:36:38 +0000

    try

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of a string
+ * @s: string to check the length
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+	return sizeof(*s);
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,4 +3,6 @@
 
 void reset_to_98(int *);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+
 #endif
