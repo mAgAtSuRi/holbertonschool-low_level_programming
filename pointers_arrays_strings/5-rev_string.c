16481d802bba8cffbba06880622a5e781fff1688
Tristan <tristan.latuilliere@gmail.com>
2025-06-24 09:20:02 +0000

    first try

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	char *revS;
+
+	while (s[i] != 0)
+		i++;
+	i--;
+	for (; i >= 0; i--)
+		revS += s[i];
+	*s = *revS
+}
