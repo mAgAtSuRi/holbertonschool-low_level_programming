9e704e91698701c5e659a66c8e68d02ab1a2ac59
Tristan <tristan.latuilliere@gmail.com>
2025-06-25 13:59:41 +0000

    try

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -12,8 +12,8 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 97 && str[i] <= 122)
-			str[i] = str[i - 32];
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
 	}
 	return (str);
 }
