78db2b51ee8938fb50cccd60acd724e4ff7705c5
Tristan <tristan.latuilliere@gmail.com>
2025-06-25 14:27:40 +0000

    test

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalize all words
+ * @str: string to capitalize
+ *
+ * Return: string capitalized
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		else if (str[i - 1] == ' ' && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
