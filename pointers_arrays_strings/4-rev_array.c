c28f1662ece418213f2b68d7ed1ea96bb0ad34a9
Tristan <tristan.latuilliere@gmail.com>
2025-06-25 13:31:44 +0000

    corrected

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,10 @@ void reverse_array(int *a, int n)
 	int i, tmp;
 	int len = 0;
 
-	while (a[len])
-		len++;
 	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
-		a[i] = a[len - 1];
-		a[len - 1] = tmp;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
 	}
 }
