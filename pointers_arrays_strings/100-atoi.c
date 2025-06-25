651922c5f6239219aa990f20e326c899201fcb76
Tristan <tristan.latuilliere@gmail.com>
2025-06-25 08:15:34 +0000

    -

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -19,8 +19,9 @@ int _atoi(char *s)
 		else if (s[i] >= 48 && s[i] <= 57)
 		{
 			if (neg % 2 != 0)
-				s[i] = - s[i];
-			tmp = tmp * 10 + (s[i] - 48);
+				tmp = -(tmp * 10 + (s[i] - 48));
+			else
+				tmp = tmp * 10 + (s[i] - 48);
 			if (s[i + 1] < 48 || s[i + 1] > 57)
 				break;
 		}
