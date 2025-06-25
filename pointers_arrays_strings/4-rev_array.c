1b26c50075612ce799e4f44d80d4e00aeebd2bd9
Tristan <tristan.latuilliere@gmail.com>
2025-06-25 13:21:42 +0000

    first try

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse content of array
+ * @a: array to reverse
+ * @n: number of elements to reverse
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+	int len = 0;
+
+	while (a[len])
+		len++;
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[len - 1];
+		a[len - 1] = tmp
+	}
+}
