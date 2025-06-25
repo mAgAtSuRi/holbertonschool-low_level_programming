433f8d0a520ae194f0a9401995b746127aeef7bc
Tristan <tristan.latuilliere@gmail.com>
2025-06-25 12:56:51 +0000

    test with \0 if no more character

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,11 +15,12 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (src[len2])
 		len2++;
-	if (n >= len2)
-		for (i = 0; i < len2; i++)
+	for (i = 0; i < n; i++)
+	{
+		if (i < len2)
 			dest[i] = src[i];
-	else
-		for (i = 0; i < n; i++)
-			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+	}
 	return (dest);
 }
