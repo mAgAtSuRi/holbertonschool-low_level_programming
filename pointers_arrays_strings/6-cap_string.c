a76262a4645eda2bad46273072cbe2197a1db398
Tristan <tristan.latuilliere@gmail.com>
2025-06-26 07:50:32 +0000

    j until 13

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ char *cap_string(char *str)
 			str[i] = str[i] - 32;
 		else if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			for (j = 0; j < 12; j++)
+			for (j = 0; j <= 12; j++)
 			{
 				if (str[i - 1] == separator[j])
 				{
