3f931fba272fab8c1c28faff1b1edf56d1dec2aa
Tristan <tristan.latuilliere@gmail.com>
2025-06-26 14:39:25 +0000

    gpt help

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -8,12 +8,7 @@ void print_number(int n)
 {
 	int i;
 
-	if (n >= 0 && n < 10)
-		_putchar(n + '0');
-	else
-		while (n > 10)
-		{
-			n = n / 10;
-			i++;
-		}p
-		_putchar(n + '0'):
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar(n % 10);
+}
