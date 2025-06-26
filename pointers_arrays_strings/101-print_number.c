8753e7bd92d621a22c757a8d9e7fd22371adbe87
Tristan <tristan.latuilliere@gmail.com>
2025-06-26 14:45:35 +0000

    test

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -6,7 +6,16 @@
  */
 void print_number(int n)
 {
+	int neg = 0;
+
+	if (n < 0)
+	{
+		neg = 1;
+		n = -n;
+	}
 	if (n / 10 != 0)
 		print_number(n / 10);
+	if (neg == 1)
+		_putchar('-');
 	_putchar(n % 10 + '0');
 }
