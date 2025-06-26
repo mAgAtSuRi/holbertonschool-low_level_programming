1a7df65187d3943d9d1cdc4fe4549d53f3424a01
Tristan <tristan.latuilliere@gmail.com>
2025-06-26 09:48:34 +0000

    test

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string in rot13
+ * @str: string to encode
+ *
+ * Return: string encoded
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char low[] = "abcdefghijklmnopqrstuvwxyz";
+	char low_rot13 = "nopqrstuvwxyzabcdefghijklm";
+	char upp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char upp_rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			if (str[i] == low[j])
+			{
+				str[i] = low_rot13[j];
+				break;
+			}
+			else if (str[i] == upp[j])
+			{
+				str[i] = upp_rot13[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
