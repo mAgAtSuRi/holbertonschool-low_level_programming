8f1c8a76b03c62f6ec2fb0621afd01f1575b859b
Tristan <tristan.latuilliere@gmail.com>
2025-06-26 14:53:15 +0000

    int min

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * print_number - prints an integer
@@ -8,8 +9,17 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		if (n == INT_MIN)
+		{
+			_putchar('-');
+			_putchar(2 + '0');
+			print_number(147483648);
+		}
+		else
+		{
+			_putchar('-');
+			n = -n;
+		}
 	}
 	if (n / 10 != 0)
 		print_number(n / 10);
