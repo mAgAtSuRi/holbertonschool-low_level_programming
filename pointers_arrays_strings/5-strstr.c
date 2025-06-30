b506f2715f0bcd837181ae94d1177aa8b5f36d54
Tristan <tristan.latuilliere@gmail.com>
2025-06-30 12:31:48 +0000

    first commit

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include 
+/**
+ * *_strstr - finds the first occurence of a string
+ * @needle: string occurence to check
+ * @haystack: string to check
+ *
+ * Return: pointer to the beginning of the string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int j;
+	int len2 = 0, i = 0;
+	int count = 0;
+	int pos;
+
+	while (haystack[i])
+		i++;
+	while (needle[len2])
+		len2++;
+	for (j = 0; j < i; j++)
+	{
+		if (haystack[j] == needle[0])
+			pos = j;
+			for (l = 0, l < len2; l++)
+			{
+				if (haystack[j] == needle[l])
+					count++;
+			}
+		if (count == len2)
+			return (haystack[pos]);
+		count = 0;
+	}
+	return (NULL);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -31,5 +31,6 @@ char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 int _strspn(char *, char *);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *, char *);
 
 #endif
