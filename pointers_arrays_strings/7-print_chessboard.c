4fee016f80eaf54a8be3862cee400dc910129c05
Tristan <tristan.latuilliere@gmail.com>
2025-06-30 13:36:41 +0000

    first try

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard
+ * @a: chessboard array 8x8
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i,j;
+
+	for (i = 0; i < 7; i++)
+	{
+		for (j = 0; j < 7; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -32,5 +32,6 @@ char *_strchr(char *, char);
 int _strspn(char *, char *);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *, char *);
+void print_chessboard(char *[]);
 
 #endif
