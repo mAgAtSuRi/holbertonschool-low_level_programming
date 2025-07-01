3a4e6e578ca81deca608ddd7d16301903edd2ea6
Tristan <tristan.latuilliere@gmail.com>
2025-07-01 12:23:59 +0000

    test

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,11 +8,6 @@
  */
 int _strlen_recursion(char *s)
 {
-        int n = 0;
-
 	if (*s != '\0')
-		n++;
-	else
-		return (n);
-	_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion( s + 1);
 }
