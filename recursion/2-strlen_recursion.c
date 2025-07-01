06860215c3ea6b71309154e4ee2c28a9b5112dac
Tristan <tristan.latuilliere@gmail.com>
2025-07-01 09:32:52 +0000

    yesy

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.c"
+
+/**
+ * _strlen_recursion(char *s)
+ * @s: string to check
+ * 
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+	
+	if (*s != '\0')
+		n++;
+	else
+		return (n);
+	_strlen_recursion(s + 1);
+}
