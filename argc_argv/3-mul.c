6c3fa672ebb80119fcad9b694976100944a58257
Tristan <tristan.latuilliere@gmail.com>
2025-07-02 13:25:16 +0000

    if else

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,6 +12,9 @@ int main(int argc, char *argv[])
 {
 	(void)argc;
 
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	if (argc != 2)
+		printf("Error");
+	else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
