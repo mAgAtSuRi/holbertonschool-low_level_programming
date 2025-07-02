d070ffa0eefa0ae37504610b6f4494b5dcf1ac5f
Tristan <tristan.latuilliere@gmail.com>
2025-07-02 13:59:30 +0000

    number changed

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 		{
 			while (argv[i][j])
 			{
-				if (argv[i][j] < 'a' || argv[i][j] > 'z')
+				if (argv[i][j] < 48 || argv[i][j] > 57)
 				{
 					printf("Error\n");
 					return (0);
