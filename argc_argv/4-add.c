cc7ea27e00202394e051021c460d6d1f671e37a5
Tristan <tristan.latuilliere@gmail.com>
2025-07-02 13:51:28 +0000

    first try

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add 2 numbers
+ * @argc: number of arguments
+ * @argv: list of strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j = 0;
+	(void)argc;
+	int result = 0
+
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			while (argv[i][j])
+			{
+				if (argv[i][j] < 'a' || argv[i][j] > 'z')
+				{
+					printf("Error\n");
+					return (0);
+				}
+			}
+		}
+		result += atoi(argc[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
