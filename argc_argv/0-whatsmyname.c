f774e7b8bdaf1141cf25b958753130f71f1bef97
Tristan <tristan.latuilliere@gmail.com>
2025-07-02 07:44:14 +0000

    description added

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - print name of the program
+ * @argc: number of arguments
+ * @argv: array of name of arguments
+ *
+ * Return: 0
+ */
 int main(int argc, char  **argv)
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
