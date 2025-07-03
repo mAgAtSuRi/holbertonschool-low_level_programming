b672edd337e2f5684b9974ea5d5600ce1f53a22b
Tristan <tristan.latuilliere@gmail.com>
2025-07-03 15:53:17 +0200

    test

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,5 +1,15 @@
 #include "main.h"
-
+/**
+ * reverse _ reverse a string
+ * @s: string to reverse
+ * 
+ * Return : the reversed string
+ */
+char* reverse(char *s)
+{
+	if (*s != '\0')
+		return (*s + reverse(s + 1));
+}
 /**
 is_palindrome - check if palindrome
 @s: string to check
@@ -8,14 +18,9 @@ Return: 1 if palindrome otherwise 0
 */
 int is_palindrome(char *s)
 {
-	int len = 0, i;
+	char *str;
 
-	while(s[len])
-		len++;
-	for (i = 0; i <= len / 2; i++)
-	{
-		if (s[i] != s[len - i])
-			return (0);
-	}
-	return (1);
+	if (reverse(s) == s)
+		return (1);
+	return (0);
 }
