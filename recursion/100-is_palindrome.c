235cd343ae3ed14a3f78c9dcf54f7222372c4073
Tristan <tristan.latuilliere@gmail.com>
2025-07-03 16:26:02 +0200

    ;

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -15,7 +15,7 @@ int strlen_recursion(char *s)
 int check_palindrome(char *s, int start, int end)
 {
 	if (start >= end)
-		return (1)
+		return (1);
 	if (s[start] != s[end])
 		return (0);
 	return (check_palindrome(s, start + 1, end - 1));
