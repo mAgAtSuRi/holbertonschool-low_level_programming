b4115566b6b15d33a76373bc2d40a3cf5a595dc2
Tristan <tristan.latuilliere@gmail.com>
2025-07-07 10:32:36 +0200

    first try

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * *_strdup - create a pointer to the string str
+ * @str: string to copy
+ *
+ * Return: a pointer to the string in a new space
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	s = malloc(sizeof(str));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < sizeof(str); i++)
+		s[i] = str[i];
+	return (s);
+}
