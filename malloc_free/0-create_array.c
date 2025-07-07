69d484c7b22961e496be6aa04c47abc90691a490
Tristan <tristan.latuilliere@gmail.com>
2025-07-07 10:09:22 +0200

    initialized the whole array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,18 +3,23 @@
 /**
  * *create_array - create an array of char
  * @size: size of the array
- * @c: first character to initialize
+ * @c: character to initialize
  * 
  * Return: pointer to the char array
  */
 char *create_array(unsigned int size, char c)
 {
 	char *s;
+	int i = 0;
 
 	if (size == 0)
 		return (NULL);
 	s = malloc(size * sizeof(c));
-	s[0] = c;
+	while (s[i])
+	{
+		s[i] = c;
+		i++;
+	}
 	if (s == NULL)
 		return (NULL);
 	return (s);
