ecdcd2a3d6a967abfb35ae686b1021fdfb6e6c3b
Tristan <tristan.latuilliere@gmail.com>
2025-07-07 11:43:48 +0200

    first try

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -5,7 +5,7 @@
  * alloc_grid - returns a pointer to a 2d array
  * @width: width
  * @length: length
- * 
+ *
  * Return: pointer to 2d array
  */
 int **alloc_grid(int width, int height)
@@ -24,45 +24,4 @@ int **alloc_grid(int width, int height)
 			ptr[i][j] = 0;
 	}
 	return (ptr);
-}
-
-void print_grid(int **grid, int width, int height)
-{
-    int w;
-    int h;
-
-    h = 0;
-    while (h < height)
-    {
-        w = 0;
-        while (w < width)
-        {
-            printf("%d ", grid[h][w]);
-            w++;
-        }
-        printf("\n");
-        h++;
-    }   
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int **grid;
-
-    grid = alloc_grid(6, 4);
-    if (grid == NULL)
-    {
-        return (1);
-    }
-    print_grid(grid, 6, 4);
-    printf("\n");
-    grid[0][3] = 98;
-    grid[3][4] = 402;
-    print_grid(grid, 6, 4);
-    return (0);
 }
