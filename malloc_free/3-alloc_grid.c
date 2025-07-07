c52fab6d6be0040249c81d39bd841896fb4c9752
Tristan <tristan.latuilliere@gmail.com>
2025-07-07 11:51:57 +0200

    ptr

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,11 +15,14 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * width);
-
+	ptr = malloc(sizeof(*ptr) * width);
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < width; i++)
 	{
-		*ptr[i] = malloc(sizeof(int) * height);
+		ptr[i] = malloc(sizeof(int) * height);
+		if (ptr[i] == NULL)
+			return (NULL);
 		for (j = 0; j <height; j++)
 			ptr[i][j] = 0;
 	}
