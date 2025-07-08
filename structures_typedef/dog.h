e19d59ffdba885ca941ad9f2a42504644c955ef4
Tristan <tristan.latuilliere@gmail.com>
2025-07-08 11:41:49 +0200

    struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,12 +2,10 @@
 #define MAIN_H
 
 /**
- * dog - strucure of a dog
+ * struct dog - strucure of a dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
- * Description: simple structure
  */
 struct dog
 {
