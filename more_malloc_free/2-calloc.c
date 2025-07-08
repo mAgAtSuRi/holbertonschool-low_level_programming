5598b888fa7a7960250d3aebfd40ce1e24c9649c
Tristan <tristan.latuilliere@gmail.com>
2025-07-08 10:07:44 +0200

    changed order

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,10 +10,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *arr;
 	unsigned int i;
 
-	arr = malloc(size * nmemb);
-
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
+	arr = malloc(size * nmemb);
 	if (arr == NULL)
 		return (NULL);
 	
