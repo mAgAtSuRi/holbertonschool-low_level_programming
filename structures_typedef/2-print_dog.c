54b886f725e8716901adb58720a029653eca345e
Tristan <tristan.latuilliere@gmail.com>
2025-07-08 13:55:51 +0200

    formatted

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,12 +10,12 @@ void print_dog(dog_t *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("nil\n");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("nil\n");
+		printf("Age: (nil)\n");
 	else
-		printf("Owner: %s\n",d->owner);
+		printf("Owner: %s\n", d->owner);
 }
