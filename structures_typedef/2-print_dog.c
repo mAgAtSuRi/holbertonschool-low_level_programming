ba8579a6e7968e8e0f6c1c461e636be59feac27e
Tristan <tristan.latuilliere@gmail.com>
2025-07-08 13:54:52 +0200

    if null then nil

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,5 +9,13 @@ void print_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	if (d->name == NULL)
+		printf("nil\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("nil\n");
+	else
+		printf("Owner: %s\n",d->owner);
 }
