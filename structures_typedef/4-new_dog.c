5b9084899f9fce220d037e905e82f389637318d2
Tristan <tristan.latuilliere@gmail.com>
2025-07-09 10:47:33 +0200

    copy of char *

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,12 +11,24 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int len_name = 0, len_owner = 0;
+	int i;
+
 	dog_t *dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = name;
+	while (name[len_name])
+		len_name++;
+	while (owner[len_owner])
+		len_owner++;
+
+	dog->name = malloc(len_name + 1);
+	for (i = 0; i < len_name; i++)
+		dog->name[i] = name[i];
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = malloc(len_owner + 1);
+	for (i = 0; i < len_owner; i++)
+		dog->owner[i] = owner[i];
 	return (dog);
 }
