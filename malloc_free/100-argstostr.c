3590925e73f58274fd09100332b41b847c97fe9a
Tristan <tristan.latuilliere@gmail.com>
2025-07-10 10:37:09 +0200

    first try

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all arguments of the program
+ * @ac: number of args to concatenate
+ * @av: pointer of an array containing the arguments
+ *
+ * Return: a pointer to thez concanated string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j = 0, len = 0, len_word;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		len_word = 0;
+		while(av[i][len_word])
+			len_word++;
+		for (j = 0; j < len_word; j++)
+			str[j + len] = av[i][j];
+		str[len_word + len] = '\n';
+		len_word += len;
+	}
+	return (str);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -7,5 +7,6 @@ char *_strdup(char *);
 char *str_concat(char *, char *);
 int **alloc_grid(int, int);
 void free_grid(int **,int);
+char *argstostr(int ac, char **av);
 
 #endif
