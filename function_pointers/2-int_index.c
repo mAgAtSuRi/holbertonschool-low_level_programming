f5e267962b68d0776ff4ec217f1a86894030ffce
Tristan <tristan.latuilliere@gmail.com>
2025-07-15 12:08:01 +0200

    first try

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search
+ * @size: size of the array
+ * @cmp: pointer to the compare function
+ *
+ * Return: the index or -1 if error/no element found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
+			return (i);
+	
+	return (-1);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -5,5 +5,6 @@
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *, size_t size, void (*action)(int));
+int int_index(int *, int, int (*cmp)(int));
 
 #endif
