0017922b6378e7931fd0f669cce65481b5570914
Tristan <tristan.latuilliere@gmail.com>
2025-07-15 15:01:31 +0200

    '

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 		return (98);
 	}
 
-	if ((argv[2][0] == "%" || argv[2][0] == "/") && argv[3][0] == "0")
+	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
 	{
 		printf("Error");
 		return (100);
