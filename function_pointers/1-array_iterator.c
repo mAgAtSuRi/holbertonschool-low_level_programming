5a498701ebbb81f3d4ebdf4855766ac47a37db5a
Tristan <tristan.latuilliere@gmail.com>
2025-07-15 11:48:04 +0200

    first try

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - executes a function on each element of array
+ * @array: array of int
+ * @size: size of the array
+ * @action: pointer to function to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(i);
+}
