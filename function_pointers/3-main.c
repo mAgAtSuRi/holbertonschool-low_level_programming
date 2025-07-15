791a433d10bb7d53f395865bc5359415ea7ad04a
Tristan <tristan.latuilliere@gmail.com>
2025-07-15 14:32:08 +0200

    strcmp

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -19,7 +19,12 @@ int main(int argc, char **argv)
 		return (98);
 	}
 	
-	if (argv[2] != "+" && argv[2] != "-" && argv[2] != "*" && argv[2] != "/" && argv[2] != '%')
+	if (
+    strcmp(argv[2], "+") != 0 &&
+    strcmp(argv[2], "-") != 0 &&
+    strcmp(argv[2], "*") != 0 &&
+    strcmp(argv[2], "/") != 0 &&
+    strcmp(argv[2], "%") != 0)
 	{
 		printf("Error\n");
 		return (99);
