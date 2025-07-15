d6179f583b80cb75a1b8957ccf1f43d2d5ae0c87
Tristan <tristan.latuilliere@gmail.com>
2025-07-15 15:08:21 +0200

    other operartor return 98

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -20,7 +20,11 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (98);
 	}
-
+	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' && argv[2][0] != '%')
+	{
+		printf("Error\n");
+		return (99);
+	}
 	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
 	{
 		printf("Error");
