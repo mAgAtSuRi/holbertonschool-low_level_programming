7adaac3de4a0adcce1bd1145eef1798b6b110b7c
Tristan <tristan.latuilliere@gmail.com>
2025-07-16 11:42:06 +0200

    va started + removed separator for last element

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,6 +16,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 		return;
-	for (i = 0; i < n; i++)
-	printf("%d%s", va_arg(args, int), separator);
+	va_start(args, n);
+	for (i = 0; i < n - 1; i++)
+		printf("%d%s", va_arg(args, int), separator);
+	printf("%d\n", va_arg(args, int));
 }
