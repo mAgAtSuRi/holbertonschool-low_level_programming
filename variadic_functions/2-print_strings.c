5d16535809be3156d63c35575e4a0e46e9cdc7e2
Tristan <tristan.latuilliere@gmail.com>
2025-07-16 14:31:17 +0200

    betty

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,8 +13,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 	char *s;
-	va_start(args, n);
 
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(args, char *);
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		else
 			printf("%s", s);
-		
+
 		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 	}
