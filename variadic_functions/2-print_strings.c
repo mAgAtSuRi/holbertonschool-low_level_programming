4be228b064e9d00fbf8d067f287b9289a3bf35e5
Tristan <tristan.latuilliere@gmail.com>
2025-07-16 14:27:04 +0200

    variable tampon

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -12,14 +12,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-
+	char *s;
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(args, char*) == NULL)
+		s = va_arg(args, char *);
+		if (s == NULL)
 			printf("(nil)");
+
 		else
-			printf("%s", va_arg(args, char*));
+			printf("%s", s);
 		
 		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
