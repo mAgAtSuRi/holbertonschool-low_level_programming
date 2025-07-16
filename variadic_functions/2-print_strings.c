fb192b59d602145fe12556e6b129cc69f0471de9
Tristan <tristan.latuilliere@gmail.com>
2025-07-16 12:02:42 +0200

    space

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,10 +16,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(args, char *) == NULL)
+		if (va_arg(args, char*) == NULL)
 			printf("(nil)");
 		else
-			printf("%s", va_arg(args, char *));
+			printf("%s", va_arg(args, char*));
 		
 		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
