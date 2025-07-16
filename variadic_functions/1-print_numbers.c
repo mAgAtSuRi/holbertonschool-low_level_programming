6571470567436055e9a1654aa7a31cbc7629f7d5
Tristan <tristan.latuilliere@gmail.com>
2025-07-16 11:50:37 +0200

    first print numb then separator

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,10 +14,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
 	va_start(args, n);
-	for (i = 0; i < n - 1; i++)
-		printf("%d%s", va_arg(args, int), separator);
-	printf("%d\n", va_arg(args, int));
+	for (i = 0; i < n ; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
 }
