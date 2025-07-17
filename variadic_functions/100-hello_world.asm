25a5869072cf56221fd57c3e4fd3f889503ede54
Tristan <tristan.latuilliere@gmail.com>
2025-07-17 14:59:28 +0200

    space between section

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,12 +1,12 @@
-section.data
+section .data
 	message db "Hello, World", 10
 section.text
-	global_start
+	global _start
 
 _start:
 	mov rax, 1
-	mov, rdi, 1
-	mov, rsi, message
+	mov rdi, 1
+	mov rsi, message
 	mov rdx, 13
 	syscall
 
