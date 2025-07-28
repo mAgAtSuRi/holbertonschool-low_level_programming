6960ad367a22d43c1cae131e39ebfde38454fcad
Tristan <tristan.latuilliere@gmail.com>
2025-07-28 10:49:46 +0200

    Firqt try on linked lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_list - prints all elements of a list
+ * @list_t: list to print
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i, nodes = 0;
+
+	while (h->next != NULL)
+	{
+		printf("%s\n", h->str);
+		nodes++;
+	}
+	return (nodes);
+}
