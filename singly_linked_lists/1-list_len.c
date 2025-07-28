3422ccb4169e8dc65c4e85899df383f6446587a0
Tristan <tristan.latuilliere@gmail.com>
2025-07-28 14:44:09 +0200

    incremented

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 /**
  * list_len - returns the number of elements in a linked list
  * @h: pointer to the first node of the list
- * 
+ *
  * Return: number of elements in the list
  */
 size_t list_len(const list_t *h)
@@ -11,6 +11,8 @@ size_t list_len(const list_t *h)
 	int n = 0;
 
 	while (h != NULL)
+	{
 		n++;
+		h = h->next;
 	return (n);
 }
