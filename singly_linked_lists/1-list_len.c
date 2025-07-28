5df55b00d7a8c5ed08d6fc6b90e061bd270de50e
Tristan <tristan.latuilliere@gmail.com>
2025-07-28 14:35:44 +0200

    first try

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the first node of the list
+ * 
+ * Return: number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	int n = 0;
+
+	while (h != NULL)
+		n++;
+	return (n);
+}
