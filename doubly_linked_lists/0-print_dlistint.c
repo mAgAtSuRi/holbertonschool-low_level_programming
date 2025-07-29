a3e1ea7baaa9220483c90601fef975d25f9f469a
Tristan <tristan.latuilliere@gmail.com>
2025-07-29 14:31:39 +0200

    new line added

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * print_dlistint - print all elements of a double list
  * @h: pointer to the head of the list
- * 
+ *
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 	int num = 0;
 	while(h != NULL)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		num++;
 		h = h->next;
 	}
