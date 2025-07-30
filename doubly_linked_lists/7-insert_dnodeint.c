1fca37352fa316e70a6796461e78b78a4325aa2c
Tristan <tristan.latuilliere@gmail.com>
2025-07-30 16:04:02 +0200

    malloc at the beginning

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h;
 	unsigned int pos = 0;
-
+	dlistint_t *node = malloc(sizeof(dlistint_t));
+	
+	if (node == NULL)
+		return (NULL);	
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
@@ -27,12 +30,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 	
-	dlistint_t *node = malloc(sizeof(dlistint_t));
-	if (node == NULL)
-		return (NULL);	
+
 	node->n = n;
 	node->prev = temp;
-	node->next = temp->next
+	node->next = temp->next;
 	temp->next->prev = node;
 	temp->next = node;
 
