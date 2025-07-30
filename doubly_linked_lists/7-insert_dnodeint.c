13de99f4292c066cb51e45345882696258e8d633
Tristan <tristan.latuilliere@gmail.com>
2025-07-30 16:04:51 +0200

    betty

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,9 +13,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = *h;
 	unsigned int pos = 0;
 	dlistint_t *node = malloc(sizeof(dlistint_t));
-	
+
 	if (node == NULL)
-		return (NULL);	
+		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
@@ -26,10 +26,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (temp == NULL)
 		return (NULL);
-	
+
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
-	
+
 
 	node->n = n;
 	node->prev = temp;
