399a78d1b2db4952cc7ff2e0fc299ba96b77ff3c
Tristan <tristan.latuilliere@gmail.com>
2025-07-30 16:34:37 +0200

    check if node is first or last

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -4,21 +4,36 @@
  * delete_dnodeint_at_index - delete node at specific index
  * @head: pointer to the pointer of the head
  * @index: index of the node to delete
- * 
+ *
  * Return: 1 if succeded else -1
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int pos = 0;
-
-	while (*head != NULL && pos != index)
-		*head = (*head)->next;
+	dlistint_t *temp = *head;
 
 	if (*head == NULL)
 		return (-1);
-	
-	(*head)->prev->next = (*head)->next->prev;
-	(*head)->next->prev = (*head)->prev->next;
-	free(*head);
+
+	while (temp!= NULL && pos < index)
+	{
+		temp = temp->next;
+		pos++;
+	}
+
+	if (temp== NULL)
+		return (-1);
+
+	if (temp->prev == NULL)
+		*head = temp->next;
+	else if (temp->next == NULL)
+		temp->prev->next = NULL;
+	else
+	{
+		temp->prev->next = temp->next;
+		temp->next->prev = temp->prev;
+	}
+
+	free(temp);
 	return (1);
 }
