b4c23cb0ea561ce588649dce8c7c24b12c74d464
Tristan <tristan.latuilliere@gmail.com>
2025-07-30 11:49:35 +0200

    betty

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,7 @@
  * get_dnodeint_at_index - return a node of a position
  * @head: pointer to the head of the list
  * @index: position of the node to return
- * 
+ *
  * Return: pointer to the specific node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
