2bc8abdf87619181fa3a52b75b009a34a7b36eb5
Tristan <tristan.latuilliere@gmail.com>
2025-07-31 14:49:25 +0200

    conditions added

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -16,12 +16,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
 	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
 	n_written = write(STDOUT_FILENO, buffer, n_read);
 	free(buffer);
 	close(fd);
