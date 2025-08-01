656ca5216e209330c2c3e5a13b5dbc80279304e1
Tristan <tristan.latuilliere@gmail.com>
2025-08-01 16:57:39 +0200

    value

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -18,7 +18,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	while (tmp != NULL)
 	{
 		if (strcmp(tmp->key, key) == 0)
-			return (tmp->vamue);
+			return (tmp->value);
 		tmp = tmp->next;
 	}
 	return (NULL);
