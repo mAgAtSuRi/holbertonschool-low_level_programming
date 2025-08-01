c8b356056a31dacc4bc5faf927b3a91a41f50887
Tristan <tristan.latuilliere@gmail.com>
2025-08-01 14:42:21 +0200

    betty

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,9 +13,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
-	
+
 	hash_table->size = size;
-	hash_table->array = calloc(size, sizeof(hash_node_t*));
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
