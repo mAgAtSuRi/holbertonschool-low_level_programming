b174fadaf3f63ad38c73f91e87eac9991a0ec43e
Tristan <tristan.latuilliere@gmail.com>
2025-08-01 16:57:21 +0200

    first try

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve value associated with a key
+ * @ht: hash table to check
+ * @key: key to look for
+ * 
+ * Return: value associated or NULL if cant find
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *tmp;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->vamue);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
