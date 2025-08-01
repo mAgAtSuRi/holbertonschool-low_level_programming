8a2ef9a9b3872247fdd78039aa58130fbe7fab46
Tristan <tristan.latuilliere@gmail.com>
2025-08-01 15:41:32 +0200

    first try

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives index of a key
+ * @key: key to check
+ * @size: size of the array of the hash table
+ *
+ * Return: Index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash = hash_djb2(key);
+	index = hash % size;
+	return (index);
+}
