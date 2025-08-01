14928238934b6f051c467f0d23cc79480192eeac
Tristan <tristan.latuilliere@gmail.com>
2025-08-01 15:42:14 +0200

    betty

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -9,7 +9,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash = hash_djb2(key);
+	unsigned long int hash = hash_djb2(key), index;
+
 	index = hash % size;
 	return (index);
 }
