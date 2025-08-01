e5033cadeb9f1055f6289176769fa798782795b7
Tristan <tristan.latuilliere@gmail.com>
2025-08-01 16:41:19 +0200

    commit back

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,37 +11,16 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node, *tmp;
 
-	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[index];
-	while(tmp->next != NULL)
-	{
-		if (tmp->key == key)
-		{
-			free(tmp);
-			tmp->value = strdup(value);
-			 if (tmp->value == NULL)
-                return (0);
-            return (1);
-		}
-		tmp = tmp->next;
-	}
-
-	node = malloc(sizeof(hash_node_t));
+	hash_node_t *node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 	
 	node->key = strdup(key);
 	node->value = strdup(value);
-	if (node->key == NULL || node->value == NULL)
-	{
-		free(node->key);
-		free(node->value);
-		free(node);
-		return (0);
-	}
+	node->next = NULL;
 
+	index = key_index((const unsigned char *)key, ht->size);
 	node->next = ht->array[index];
 	ht->array[index] = node;
 
